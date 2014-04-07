772946f30c0e05818735a2a7730bf1474fd002af
Radim Re <radim.res@gmail.com>
2014-04-07 16:08:58 +0200

    add create and use db to banka.sql
    add null attributes and changed name of column limit to value_limit to banka_v2.sql

--- db/banka_v2.sql
+++ db/banka_v2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.4
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Stř 26. bře 2014, 18:59
--- Verze serveru: 5.6.15-log
--- Verze PHP: 5.5.8
+-- Vygenerováno: Pon 07. dub 2014, 16:03
+-- Verze serveru: 5.6.11
+-- Verze PHP: 5.5.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,7 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Databáze: `banka`
 --
-
 CREATE DATABASE IF NOT EXISTS `banka` DEFAULT CHARACTER SET utf8 COLLATE utf8_czech_ci;
 USE `banka`;
 
@@ -51,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `account` (
 CREATE TABLE IF NOT EXISTS `account_type` (
   `id_account_type` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) COLLATE utf8_czech_ci NOT NULL,
-  `description` varchar(1000) COLLATE utf8_czech_ci NOT NULL,
+  `description` varchar(1000) COLLATE utf8_czech_ci DEFAULT NULL,
   PRIMARY KEY (`id_account_type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
 
@@ -69,8 +68,8 @@ CREATE TABLE IF NOT EXISTS `client` (
   `address2` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   `postal_code` varchar(5) COLLATE utf8_czech_ci NOT NULL,
   `personal_number` varchar(10) COLLATE utf8_czech_ci NOT NULL,
-  `telephone` varchar(13) COLLATE utf8_czech_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8_czech_ci NOT NULL,
+  `telephone` varchar(13) COLLATE utf8_czech_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8_czech_ci DEFAULT NULL,
   PRIMARY KEY (`id_client`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
 
@@ -84,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `delegated_person` (
   `id_delegated_person` int(11) NOT NULL AUTO_INCREMENT,
   `id_client` int(11) NOT NULL,
   `id_account` int(11) NOT NULL,
-  `limit` int(11) NOT NULL,
+  `value_limit` int(11) NOT NULL,
   PRIMARY KEY (`id_delegated_person`),
   KEY `id_client` (`id_client`),
   KEY `id_account` (`id_account`)
@@ -98,21 +97,28 @@ CREATE TABLE IF NOT EXISTS `delegated_person` (
 
 CREATE TABLE IF NOT EXISTS `employee` (
   `id_employee` int(11) NOT NULL AUTO_INCREMENT,
-  `id_role` int(11) NOT NULL,
+  `id_role` int(11) DEFAULT NULL,
   `name` varchar(20) COLLATE utf8_czech_ci NOT NULL,
   `surname` varchar(30) COLLATE utf8_czech_ci NOT NULL,
   `address1` varchar(30) COLLATE utf8_czech_ci NOT NULL,
   `address2` varchar(30) COLLATE utf8_czech_ci NOT NULL,
   `postal_code` varchar(5) COLLATE utf8_czech_ci NOT NULL,
-  `telephone` varchar(13) COLLATE utf8_czech_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8_czech_ci NOT NULL,
+  `telephone` varchar(13) COLLATE utf8_czech_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8_czech_ci DEFAULT NULL,
   `login` varchar(20) COLLATE utf8_czech_ci NOT NULL,
   `password` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_employee`),
   UNIQUE KEY `login` (`login`),
   KEY `id_role` (`id_role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=2 ;
+
+--
+-- Vypisuji data pro tabulku `employee`
+--
+
+INSERT INTO `employee` (`id_employee`, `id_role`, `name`, `surname`, `address1`, `address2`, `postal_code`, `telephone`, `email`, `login`, `password`, `active`) VALUES
+(1, NULL, 'test', '', '', '', '', NULL, NULL, '', 't', 1);
 
 -- --------------------------------------------------------
 
@@ -127,12 +133,12 @@ CREATE TABLE IF NOT EXISTS `operation` (
   `id_operation_type` int(11) NOT NULL,
   `target_account_number` int(11) NOT NULL,
   `bank_number` int(11) NOT NULL,
-  `variable_symbol` int(11) NOT NULL,
-  `specific_symbol` int(11) NOT NULL,
-  `constant_symbol` int(11) NOT NULL,
+  `variable_symbol` int(11) DEFAULT NULL,
+  `specific_symbol` int(11) DEFAULT NULL,
+  `constant_symbol` int(11) DEFAULT NULL,
   `date` date NOT NULL,
   `value` int(11) NOT NULL,
-  `message` varchar(1000) COLLATE utf8_czech_ci NOT NULL,
+  `message` varchar(1000) COLLATE utf8_czech_ci DEFAULT NULL,
   PRIMARY KEY (`id_operation`),
   KEY `id_delegated_person` (`id_delegated_person`),
   KEY `id_employee` (`id_employee`),
@@ -160,7 +166,7 @@ CREATE TABLE IF NOT EXISTS `operation_type` (
 CREATE TABLE IF NOT EXISTS `role` (
   `id_role` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) COLLATE utf8_czech_ci NOT NULL,
-  `description` varchar(1000) COLLATE utf8_czech_ci NOT NULL,
+  `description` varchar(1000) COLLATE utf8_czech_ci DEFAULT NULL,
   PRIMARY KEY (`id_role`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
 
@@ -192,8 +198,8 @@ ALTER TABLE `employee`
 -- Omezení pro tabulku `operation`
 --
 ALTER TABLE `operation`
-  ADD CONSTRAINT `fk_id_employee` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id_employee`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_id_delegated_person` FOREIGN KEY (`id_delegated_person`) REFERENCES `delegated_person` (`id_delegated_person`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_id_employee` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id_employee`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_id_operation_type` FOREIGN KEY (`id_operation_type`) REFERENCES `operation_type` (`id_operation_type`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
