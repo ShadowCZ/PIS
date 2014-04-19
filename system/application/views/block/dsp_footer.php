dbd9ff96afd4744a35843e27c3ae3b756de9cad9
Simicek <ondrejsimicek@seznam.cz>
2014-04-19 19:58:50 +0200

    Added JS files, created new design

--- system/application/views/block/dsp_footer.php
+++ system/application/views/block/dsp_footer.php
@@ -1,6 +1,11 @@
-	<p class="footer">
+
+<push></push>
+<footer class="row">
+    <div class="well text-right col-md-12">
         System is powered by: <b>© 2014 PIS Team (Radim Reš, Martin Foukal, Tomáš Smetka, Ondřej Šimíček)</b>
-    </p>
+    </div>
+</footer>
+
 </div>
 
 </body>
--- system/application/views/dsp_login.php
+++ system/application/views/dsp_login.php
@@ -1,15 +1,32 @@
-<div id="container">
-	<h1>Banka</h1>
-
-	<div id="body">
-        <p align="right">Ukázka jak psát odkazy:{anchor uri="cmain/registration/" label="Registrace"}</p>
-        <h2>Login:</h2>
+<header class="col-md-12">
+    <nav class="navbar navbar-default col-md-6 col-md-offset-3" role="navigation">    
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <h3>
+                        Důvěryhodná banka
+                    </h3>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<content class="col-md-12 container">
+    <div class="col-md-offset-4 col-md-4">
         <form method="post" action="{site_url('cmain/doLogin')}">
-            <table>
-                <tr><th>Login</th><td><input name="login" type="text"></td></tr>
-                <tr><th>Heslo</th><td><input name="pass" type="password"></td></tr>
-                <tr><td><td align="right"><input type="submit" value="Přihlásit" name="submit"></td></tr>
-            </table>
+            <div class="form-group">
+                <label for="exampleInputLogin">Login</label>
+                <input  name="login" type="text" class="form-control" id="exampleInputLogin" placeholder="Login">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPass">Heslo</label>
+                <input  name="pass" type="password"class="form-control" id="exampleInputPass" placeholder="Password" value="t">
+            </div>
+            <div class="text-right">    {anchor uri="cmain/registration/" label="Registrace"}</div>
+            <div class="text-right">
+                <button type="submit" class="btn btn-default">Přihlásit</button>
+            </div>
         </form>
-	</div>
-</div>
+    </div>
+</content>
+
