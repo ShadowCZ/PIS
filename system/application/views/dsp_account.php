c5ca1094ad90b3bb52ebcdb4adb4edf2912b93b3
Radim Res <radim.res@gmail.com>
2014-04-29 21:44:06 +0200

    - add empty views account, account_list, client, client_list

---
+++ system/application/views/dsp_account.php
@@ -0,0 +1,7 @@
+<div class="row clearfix">
+    <div class="col-md-3 column">
+        {*include file="./block/dsp_menu_admin.php"*}
+    </div>
+    {$oAccount->ID}
+    
+</div>
---
+++ system/application/views/dsp_client_list.php
@@ -0,0 +1,8 @@
+<div class="row clearfix">
+    <div class="col-md-3 column">
+        {*include file="./block/dsp_menu_admin.php"*}
+    </div>
+    {foreach $aClients as $oClient}
+    {/foreach}
+    
+</div>
