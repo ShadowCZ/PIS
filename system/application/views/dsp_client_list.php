7098de59e383883b8a9467c3b2f6d67e1b52888a
Simicek <ondrejsimicek@seznam.cz>
2014-04-30 21:40:31 +0200

    Added clientList template

--- system/application/views/dsp_client_list.php
+++ system/application/views/dsp_client_list.php
@@ -1,8 +1,117 @@
 <div class="row clearfix">
     <div class="col-md-3 column">
-        {*include file="./block/dsp_menu_admin.php"*}
+        {include file="./block/dsp_menu_admin.php"}
     </div>
-    {foreach $aClients as $oClient}
-    {/foreach}
-    
+    <div class="col-md-9 column">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    Seznam zaměstnanců
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <form class="form-inline col-md-6" role="form">
+                          <div class="form-group">
+                            <label class="sr-only" for="searchText">Email address</label>
+                            <input type="text" class="form-control" id="searchText" name="searchText" placeholder="Text">
+                          </div>
+                          <button type="submit" class="btn btn-default">Hledat</button>
+
+                    </form>
+                    <div class=" col-md-6">
+                        <a href="{site_url('cadmin/showClientCreate')}/">
+                            <button  class="btn btn-success col-md-offset-5">Přidat záznam</button>
+                        </a>
+                    </div>
+                </div>
+
+
+                <table class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>
+                                Jméno
+                            </th>
+                            <th>
+                                Příjmení
+                            </th>
+                            <th>
+                                Telefon
+                            </th>
+                            <th>
+                                Email
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {foreach $aClients as $oClient}
+                            <!-- BUG: vim, ze ta adresa je blbe, nevim jak ji udelat dobre -->
+                            <tr  class='clickableRow' href="{site_url('cadvice/showClientEdit')}/{$aClients->ID}">cadvice/showAccountList/$iClient
+                                <td>
+                                    {$aClients->ID}
+                                </td>
+                                <td>
+                                    {$aClients->name}
+                                </td>
+                                <td>
+                                    {$aClients->surname}
+                                </td>
+                                <td>
+                                    {$aClients->tel}
+                                </td>
+                                <td>
+                                    {$aClients->email}
+                                </td>
+                                <td class="dont-select">
+                                    <a href="{site_url('cadmin/removeClient')}/{$aClients->ID}">
+                                        <button type="button" class="btn btn-danger btn-md">
+                                            <span class="glyphicon glyphicon-remove"></span>
+                                        </button>
+                                    </a>
+                                </td>
+                            </tr>
+                        {/foreach}
+                    </tbody>
+                </table>
+                {* Přidáme až při rozšiřování
+                <div class="row clearfix">
+                    <div class="col-md-offset-8 col-md-4 column ">
+                        <ul class="pagination pagination-sm">
+                            <li>
+                                <a href="#">Prev</a>
+                            </li>
+                            <li>
+                                <a href="#">1</a>
+                            </li>
+                            <li>
+                                <a href="#">2</a>
+                            </li>
+                            <li>
+                                <a href="#">3</a>
+                            </li>
+                            <li>
+                                <a href="#">4</a>
+                            </li>
+                            <li>
+                                <a href="#">5</a>
+                            </li>
+                            <li>
+                                <a href="#">Next</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                *}
+            </div>
+<!--                         <div class="panel-footer">
+                Panel footer
+            </div> -->
+        </div>
+        
+        
+    </div>
 </div>
