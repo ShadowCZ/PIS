713c2b5f05f5966f82c4c49dc900e71456bc6ef7
Simicek <ondrejsimicek@seznam.cz>
2014-05-05 17:54:56 +0200

    Modified required inputs

--- system/application/views/dsp_client.php
+++ system/application/views/dsp_client.php
@@ -29,19 +29,19 @@
                             <div class="form-group">
                                 <label for="input2" class="col-sm-2 control-label">Telefon</label>
                                 <div class="col-sm-10">
-                                    <input required title="Toto pole je potřeba vyplnit." type="text" class="form-control" id="input2" name="tel" placeholder="Telefon" value="{if $oClient->tel}{$oClient->tel}{/if}">
+                                    <input type="text" class="form-control" id="input2" name="tel" placeholder="Telefon" value="{if $oClient->tel}{$oClient->tel}{/if}">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="input5" class="col-sm-2 control-label">Město</label>
                                 <div class="col-sm-10">
-                                    <input required title="Toto pole je potřeba vyplnit."  type="text" class="form-control" id="input5" name="address1" placeholder="Město" value="{if $oClient->address1}{$oClient->address1}{/if}">
+                                    <input type="text" class="form-control" id="input5" name="address1" placeholder="Město" value="{if $oClient->address1}{$oClient->address1}{/if}">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="input7" class="col-sm-2 control-label">PSČ</label>
                                 <div class="col-sm-10">
-                                    <input required title="Toto pole je potřeba vyplnit." type="text" class="form-control" id="input7" name="postalCode" placeholder="PSČ" value="{if $oClient->postalCode}{$oClient->postalCode}{/if}">
+                                    <input type="text" class="form-control" id="input7" name="postalCode" placeholder="PSČ" value="{if $oClient->postalCode}{$oClient->postalCode}{/if}">
                                 </div>
                             </div>
                         </div>
@@ -55,13 +55,13 @@
                             <div class="form-group">
                                 <label for="input4" class="col-sm-2 control-label">Email</label>
                                 <div class="col-sm-10">
-                                    <input required title="Toto pole je potřeba vyplnit." type="email" class="form-control" id="input4" name="email" placeholder="Email"  value="{if $oClient->email}{$oClient->email}{/if}">
+                                    <input type="email" class="form-control" id="input4" name="email" placeholder="Email"  value="{if $oClient->email}{$oClient->email}{/if}">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="input6" class="col-sm-2 control-label">Ulice</label>
                                 <div class="col-sm-10">
-                                    <input required title="Toto pole je potřeba vyplnit." type="text" class="form-control" id="input6" name="address2" placeholder="Ulice" value="{if $oClient->address2}{$oClient->address2}{/if}">
+                                    <input type="text" class="form-control" id="input6" name="address2" placeholder="Ulice" value="{if $oClient->address2}{$oClient->address2}{/if}">
                                 </div>
                             </div>
                         </div>
