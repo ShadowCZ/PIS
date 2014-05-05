33b98101da951b7448e7d9054c39fc5af14e3928
Simicek <ondrejsimicek@seznam.cz>
2014-05-05 21:19:41 +0200

    Edited doc.html

--- system/application/views/block/dsp_footer.php
+++ system/application/views/block/dsp_footer.php
@@ -3,7 +3,7 @@
 <push></push>
 <footer class="row">
     <div class="well text-right col-md-12">
-        System is powered by: <b>© 2014 PIS Team (Radim Reš, Martin Foukal, Tomáš Smetka, Ondřej Šimíček)</b>
+        System is powered by: <b>© 2014 PIS Team (Radim Reš, Tomáš Smetka, Ondřej Šimíček)</b>
     </div>
 </footer>
 
